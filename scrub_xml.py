c02539b6b222548236b86700622ce56e8317bc40
mpham19 <75769500+mpham19@users.noreply.github.com>
2022-06-17 09:41:09 -0400

    Updated XML Scrubbing

--- scrub_xml.py
+++ scrub_xml.py
@@ -5,19 +5,10 @@ Created on Thu Jun 16 19:18:47 2022
 
 @author: glennturner
 """
+#set your working directory to be the git repo .../GitHub/hard-headed
 
 import xml.etree.ElementTree as ET
-import xml
 import os
-
-tree = ET.parse('SODA Dataset/Annotations/hd02.xml')
-root = tree.getroot()
-    
-#hunt through xml file and remove objects which we don't want    
-for object in root.findall('object'):
-    print(object[0].text)
-    if object[0].text not in ['person','helmet', 'vest']:
-        root.remove(object)
 
 #make a new directory if one isn't there to save the new xml files into 
 if os.path.isdir('SODA Dataset/Cleaned_Annotations'):
@@ -26,5 +17,33 @@ else:
     print('creating cleaned annotations directory')
     os.mkdir('SODA Dataset/Cleaned_Annotations')
     
-#save the xml file
-tree.write('SODA Dataset/Cleaned_Annotations/hd02.xml')
+#initialize a couple of tracking tools    
+scrubbed_count=0
+empty_xml_count=0
+empty_xml_files=[]
+
+#make a list of all the files in our dataset
+for filename in os.listdir('SODA Dataset/Annotations'):
+    
+    #if it's an xml file, enter the cleaning stage
+    if filename.endswith('.xml'):
+        
+        #parse the xml file
+        tree = ET.parse(f'SODA Dataset/Annotations/{filename}')
+        root = tree.getroot()
+            
+        #hunt through xml file and remove objects which we don't want    
+        for object in root.findall('object'):
+            #print(object[0].text)
+            if object[0].text not in ['person','helmet','vest']:
+                root.remove(object)
+            
+        #check to see if we deleted all the tags from a file
+        if len(root.findall('object'))<1:
+            print(f'All object tags removed from {filename}')
+            empty_xml_count+=1
+            empty_xml_files.append(filename)
+            
+        #save the scrubbed xml file
+        tree.write(f'SODA Dataset/Cleaned_Annotations/{filename}')
+        scrubbed_count+=1
